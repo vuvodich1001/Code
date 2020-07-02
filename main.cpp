e304f63a6b2143797e495124107d0a087a9f08d5
Vu Nguyen <vunguyen.311001@gmail.com>
2020-07-02 16:11:36 +0700

    them file main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout << "hello world\n";
+    for(int i = 0; i < 5; i++){
+        cout << "Hello World\n" << endl;
+    }
     return 0;
 }
