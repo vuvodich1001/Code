4a5b7da2e585817cc6e7b40d7dd8e7397e5e015e
Vu Nguyen <vunguyen.311001@gmail.com>
2020-07-02 16:09:23 +0700

    sua file main

--- main.cpp
+++ main.cpp
@@ -1,34 +1,56 @@
 #include <iostream>
 using namespace std;
-class Person {
-protected:
+struct node {
     int val;
-    string name;
-
-public:
-    void Nhap();
-    void Xuat();
+    node* right;
+    node* left;
 };
-int main() {
-    int n;
-    cin >> n;
-    int* a = new int[n];
-    for (int i = 0; i < n; i++) {
-        cout << i;
+node* create_node(int val) {
+    node* p = new node;
+    p->val = val;
+    p->left = p->right = NULL;
+    return p;
+}
+node* add_node(node* root, int val) {
+    if (root == NULL) {
+        root = create_node(val);
     }
+    else if (val < root->val) {
+        root->left = add_node(root->left, val);
+    }
+    else if (val > root->val) {
+        root->right = add_node(root->right, val);
+    }
+    return root;
+}
+node* search(node* root, int val) {
+    if (root == NULL) {
+        return NULL;
+    }
+    else if (val < root->val) {
+        return search(root->left, val);
+    }
+    else if (val > root->val) {
+        return search(root->right, val);
+    }
+    else {
+        return root;
+    }
+}
+int main() {
+    node* root = NULL;
+    int x, val;
     while (true) {
-        int x;
         cin >> x;
-        if (x == 0) {
-            cout << "hello world\n";
-        }
-        else if (x == 1) {
-            cout << "Duoc roi ne\n";
-        }
-        else if (x == 2) {
-            cout << "dc cua lo\n";
-        }
-        else if (x == 0) {
+        if (x == 0) break;
+        switch (x) {
+        case 1:
+            cin >> val;
+            root = add_node(root, val);
+            break;
+        case 2:
+            cin >> val;
+            cout << ((search(root, val) == NULL) ? "NO" : "YES") << endl;
             break;
         }
     }
