1ddf37e613533ffe3cd5a4510dee34d0668cc81f
Vu Nguyen <vunguyen.311001@gmail.com>
2020-07-03 10:55:00 +0700

    add file

--- DEMO/demo.cpp
+++ DEMO/demo.cpp
@@ -1,18 +1,66 @@
-#include <algorithm>
 #include <iostream>
-#include <vector>
 using namespace std;
-int main() {
-    vector<int> a;
-    int x;
-    while (true) {
-        cin >> x;
-        if (x == 0) break;
-        a.push_back(x);
+#define max 100
+struct node {
+    int val;
+    node* next;
+};
+struct list {
+    node* head;
+    node* tail;
+    list() {
+        head = tail = NULL;
     }
-    sort(a.begin(), a.end());
-    for (int i = 0; i < a.size(); i++) {
-        cout << a[i] << " ";
+};
+node* create_node(int val) {
+    node* p = new node;
+    p->val = val;
+    p->next = NULL;
+    return p;
+}
+bool search(list*& l, int val) {
+    for (node* p = l->head; p != NULL; p = p->next) {
+        if (p->val == val)
+            return true;
+    }
+    return false;
+}
+void add_node(list*& l, int val) {
+    node* p = create_node(val);
+    if (l->head == NULL) {
+        l->head = l->tail = p;
+    }
+    else {
+        l->tail->next = p;
+        l->tail = p;
+    }
+}
+void insert(list* l[], int val) {
+    int k = max % val;
+    if (search(l[k], val)) {
+        return;
+    }
+    else {
+        add_node(l[k], val);
+    }
+}
+int main() {
+    int n;
+    cin >> n;
+    list* l[max];
+    int selection, val;
+    while (true) {
+        cin >> selection;
+        if (selection == 0) break;
+        if (selection == 1) {
+            cin >> val;
+            insert(l, val);
+        }
+        // else if (selection == 2) {
+        //     cin >> val;
+        //     int k = max % val;
+        //     cout << (search(l[k], val) == true ? "Yes" : "No") << endl;
+        // }
     }
     return 0;
 }
