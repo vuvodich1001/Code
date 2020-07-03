93ac8c21fa02f2469b09202f6d242ec666d7950b
Vu Nguyen <vunguyen.311001@gmail.com>
2020-07-03 09:48:03 +0700

    add file

--- DEMO/demo.cpp
+++ DEMO/demo.cpp
@@ -1,38 +1,17 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
-int lomotu_partition(int* a, int l, int r) {
-    int pivot = a[l];
-    int i = l - 1, j = r + 1;
-    while (true) {
-        do {
-            i++;
-        } while (a[i] < pivot);
-        do {
-            j--;
-        } while (a[j] > pivot);
-        if (i >= j) {
-            return j;
-        }
-        swap(a[i], a[j]);
-    }
-}
-void quick_sort(int* a, int l, int r) {
-    if (l < r) {
-        int i = lomotu_partition(a, l, r);
-        quick_sort(a, l, i);
-        quick_sort(a, i + 1, r);
-    }
-}
 int main() {
-    int n;
-    cin >> n;
-    int* a = new int[n];
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
+    vector<int> a;
+    int x;
+    while (true) {
+        cin >> x;
+        if (x == 0) break;
+        a.push_back(x);
     }
-    quick_sort(a, 0, n - 1);
-    for (int i = 0; i < n; i++) {
+    sort(a.begin(), a.end());
+    for (int i = 0; i < a.size(); i++) {
         cout << a[i] << " ";
     }
     return 0;
