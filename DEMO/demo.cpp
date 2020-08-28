da18b85d0bcc6f614b226c14a46199f131d455f1
vuvodich1001 <62141900+vuvodich1001@users.noreply.github.com>
2020-08-28 08:44:01 +0700

    Update demo.cpp

--- DEMO/demo.cpp
+++ DEMO/demo.cpp
@@ -1,24 +1,6 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main() {
-    int n;
-    cin >> n;
-    int* a = new int[n];
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    // sap xep
-    for (int i = 0; i < n; i++) {
-        int min = i;
-        for (int j = 0; j < n; j++) {
-            if (a[j] < a[min]) {
-                min = j;
-            }
-        }
-        swap(a[i], a[min]);
-    }
-    for (int i = 0; i < n; i++) {
-        cout << a[i] << " ";
-    }
+int main(){
+    cout << "Hello World";
     return 0;
 }
