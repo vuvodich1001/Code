2a45ad08c59b635cf3b0403babcc1e8df81630a9
Vu Nguyen <vunguyen.311001@gmail.com>
2020-08-28 08:42:44 +0700

    add file

--- DEMO/demo.cpp
+++ DEMO/demo.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "hello world\n";
+    int n;
+    cin >> n;
+    int* a = new int[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    // sap xep
+    for (int i = 0; i < n; i++) {
+        int min = i;
+        for (int j = 0; j < n; j++) {
+            if (a[j] < a[min]) {
+                min = j;
+            }
+        }
+        swap(a[i], a[min]);
+    }
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
     return 0;
 }
