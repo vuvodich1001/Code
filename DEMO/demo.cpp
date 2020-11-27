de50d2502bcc3e20bec069fe324d0ae3ecee658a
Vu Nguyen <vunguyen.311001@gmail.com>
2020-11-27 09:27:39 +0700

    add file

--- DEMO/demo.cpp
+++ DEMO/demo.cpp
@@ -1,6 +1,61 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    cout << "Hello World";
+struct node {
+    int val;
+    node* left;
+    node* right;
+};
+
+node* create_node(int val) {
+    node* p = new node;
+    p->val = val;
+    p->left = p->right = NULL;
+    return p;
+}
+
+node* add_node(node* root, int val) {
+    if (root == NULL) {
+        root = create_node(val);
+    }
+    else if (val < root->val) {
+        root->left = add_node(root->left, val);
+    }
+    else if (val > root->val) {
+        root->right = add_node(root->right, val);
+    }
+    return root;
+}
+
+node* research(node* root, int val) {
+    if (root == NULL) {
+        return NULL;
+    }
+    else if (val < root->val) {
+        return research(root->left, val);
+    }
+    else if (val > root->val) {
+        return research(root->right, val);
+    }
+    else
+        return root;
+}
+int main() {
+    cout << "Hello World\n";
+    node* root = NULL;
+    int val;
+    while (true) {
+        cin >> val;
+        if (val == 1) {
+            cin >> val;
+            root = add_node(root, val);
+        }
+        else if (val == 2) {
+            cin >> val;
+            cout << ((research(root, val) == NULL) ? "NO" : "YES") << endl;
+        }
+        else if (val == 0) {
+            break;
+        }
+    }
     return 0;
 }
