0a6b9991ff1a8e20e3d3193285d6dadff43d19a8
Your Name <vunguyen.311001@gmail.com>
2020-12-23 15:19:40 +0700

    add file

--- DEMO/demo.cpp
+++ DEMO/demo.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hello World\n";
+    int n;
+    cout << "nhap gia tri n: ";
+    cin >> n;
+    int* a = new int[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cout << "\nXuat\n";
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << "hello world\n";
     return 0;
 }
