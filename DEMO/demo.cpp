69bccc1edfe83803a18647677bfe8a1a1f6a9031
Vuvodich1001 <vunguyen.311001@gmail.com>
2021-01-15 11:07:59 +0700

    add file

--- DEMO/demo.cpp
+++ DEMO/demo.cpp
@@ -1,35 +1,33 @@
+//PhanSO
 #include <iostream>
 using namespace std;
+
+class PhanSo {
+private:
+    int a;
+    int b;
+
+public:
+    void Nhap();
+    void Xuat();
+    PhanSo operator+(PhanSo a);
+    PhanSo operator+(int a);
+};
+
+void PhanSo::Nhap() {
+    cout << "Tu so: ";
+    cin >> a;
+    cout << "Mau so: ";
+    cin >> b;
+}
+
+void PhanSo::Xuat() {
+    cout << "Phan so: " << a << "/" << b << endl;
+}
+
 int main() {
-    cout << "hello world\n";
-    int n;
-    cout << "nhap gia tri n: ";
-    cin >> n;
-    int* a = new int[n];
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    cout << "\nXuat\n";
-    for (int i = 0; i < n; i++) {
-        cout << a[i] << " ";
-    }
-    cout << "hello world\n";
-    cout << "Hello World\n";
-    node* root = NULL;
-    int val;
-    while (true) {
-        cin >> val;
-        if (val == 1) {
-            cin >> val;
-            root = add_node(root, val);
-        }
-        else if (val == 2) {
-            cin >> val;
-            cout << ((research(root, val) == NULL) ? "NO" : "YES") << endl;
-        }
-        else if (val == 0) {
-            break;
-        }
-    }
+    PhanSo a;
+    a.Nhap();
+    a.Xuat();
     return 0;
 }
