43b4fc19a32d3a5f76294c2a5e69e89e138e012f
Vuvodich1001 <vunguyen.311001@gmail.com>
2021-01-17 18:09:45 +0700

    add file

--- DEMO/demo.cpp
+++ DEMO/demo.cpp
@@ -1,33 +1,87 @@
-//PhanSO
+//PhanSo
 #include <iostream>
 using namespace std;
-
 class PhanSo {
 private:
-    int a;
-    int b;
+    int tuso;
+    int mauso;
 
 public:
     void Nhap();
     void Xuat();
     PhanSo operator+(PhanSo a);
     PhanSo operator+(int a);
+    PhanSo Rut_Gon();
+    bool operator==(PhanSo a);
+    friend istream& operator>>(istream& is, PhanSo& a);
+    friend ostream& operator<<(ostream& os, PhanSo a);
 };
-
 void PhanSo::Nhap() {
-    cout << "Tu so: ";
-    cin >> a;
-    cout << "Mau so: ";
-    cin >> b;
+    cout << "Nhap phan so: ";
+    cin >> this->tuso >> this->mauso;
 }
 
 void PhanSo::Xuat() {
-    cout << "Phan so: " << a << "/" << b << endl;
+    cout << "Phan so: " << this->tuso << "/" << this->mauso;
 }
 
+PhanSo PhanSo::operator+(PhanSo a) {
+    PhanSo temp;
+    temp.tuso = this->tuso * a.mauso + a.tuso * this->mauso;
+    temp.mauso = this->mauso * a.mauso;
+    return temp;
+}
+
+PhanSo PhanSo::operator+(int a) {
+    PhanSo temp;
+    temp.tuso = this->tuso + a * this->mauso;
+    temp.mauso = this->mauso;
+    return temp;
+}
+
+istream& operator>>(istream& is, PhanSo& a) {
+    is >> a.tuso >> a.mauso;
+    return is;
+}
+
+ostream& operator<<(ostream& os, PhanSo a) {
+    os << "Phan so: " << a.tuso << "/" << a.mauso;
+    return os;
+}
+
+bool PhanSo::operator==(PhanSo a) {
+    return ((float)this->tuso / this->mauso) == ((float)a.tuso / a.mauso);
+}
+
+int UCLN(int a, int b) {
+    for (int i = a; i >= 0; i--) {
+        if (a % i == 0 && b % i == 0) {
+            return i;
+        }
+    }
+}
+
+PhanSo PhanSo::Rut_Gon() {
+    int temp = UCLN(this->tuso, this->mauso);
+    this->tuso /= temp;
+    this->mauso /= temp;
+    return *this;
+}
 int main() {
     PhanSo a, b;
-    a.Nhap();
-    a.Xuat();
+    cout << "Nhap phan so a: ";
+    cin >> a;
+    cout << a;
+    cout << "\nNhap phan so b: ";
+    cin >> b;
+    cout << b;
+    PhanSo temp = a + b + 1;
+    cout << "\nTong 2 phan so la: " << temp.Rut_Gon() << endl;
+    if (a == b) {
+        cout << "\n2 Phan so bang nhau";
+    }
+    else {
+        cout << "\n2 Phan So khong bang nhau";
+    }
     return 0;
 }
